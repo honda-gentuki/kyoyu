6d4f59b23e3b1d9b92708f4af83be4f41417b5cc
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-05 08:16:30 +0900

    デフォルトの記述を削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,8 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start()
+// jsが発火しない恐れがあるため
 require("@rails/activestorage").start()
 require("channels")
 
