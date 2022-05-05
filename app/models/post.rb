ad0cf6ed1417d33e6256dea5f76a7f3510826fe1
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 00:04:41 +0900

    Postバリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,23 @@
 class Post < ApplicationRecord
-
   belongs_to :user
-  
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :subject
+    validates :course
+    validates :unit
+    validates :teaching_materials
+  end
+
+  with_options presence: true do
+    validates :introduction, length: { maximum: 300 }
+    validates :development,  length: { maximum: 1000 }
+    validates :summary,      length: { maximum: 300 }
+  end
+
+  with_options numericality: { only_integer: true } do
+    validates :introduction_time
+    validates :development_time
+    validates :summary_time
+  end
 end
