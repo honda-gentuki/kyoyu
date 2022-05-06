7266a5d64fc6c97c649071c8be70b536e98ca846
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 15:15:11 +0900

    ログインしていないユーザーを新規投稿画面に遷移させない記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!, only: :new
+
   def index
     @users = User.all
     @posts = Post.all
@@ -23,4 +26,8 @@ private
 def post_params
   params.require(:post).permit(:subject, :course, :unit, :teaching_materials, :introduction, :introduction_time, :development,
                                :development_time, :summary, :summary_time, :image).merge(user_id: current_user.id)
+end
+
+def move_to_index
+  redirect_to new_user_session_path unless user_signed_in?
 end
