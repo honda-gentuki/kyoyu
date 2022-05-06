d3b6b59eec141b6d6942286d73e9283c2bbe1732
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 16:03:16 +0900

    授業時間を50分以内に修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,7 @@ class Post < ApplicationRecord
   validates :development,  length: { maximum: 1000 }
   validates :summary,      length: { maximum: 300 }
 
-  with_options numericality: { only_integer: true } do
+  with_options numericality: { only_integer: true, less_than_or_equal_to: 50 } do
     validates :introduction_time
     validates :development_time
     validates :summary_time
