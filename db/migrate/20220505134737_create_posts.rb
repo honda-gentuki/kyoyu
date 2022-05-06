852fbb61e7908be9aa7ce76b0f1aebf26d956e01
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 12:54:35 +0900

    バリデーション修正

--- db/migrate/20220505134737_create_posts.rb
+++ db/migrate/20220505134737_create_posts.rb
@@ -4,12 +4,12 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.string     :subject,            null: false
       t.string     :course,             null: false
       t.string     :unit,               null: false
-      t.string     :introduction,       null: false
-      t.integer    :introduction_time,  null: false
-      t.text       :development,        null: false
-      t.integer    :development_time,   null: false
-      t.string     :summary,            null: false
-      t.integer    :summary_time,       null: false
+      t.string     :introduction
+      t.integer    :introduction_time
+      t.text       :development
+      t.integer    :development_time
+      t.string     :summary
+      t.integer    :summary_time
       t.string     :teaching_materials, null: false
       t.references :user,               null: false, foreign_key: true
       t.timestamps
