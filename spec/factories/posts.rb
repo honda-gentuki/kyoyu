795ec2296e6d3dbc6f1035093214f96da3ce0c6c
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 16:36:43 +0900

    postのFactoryBot作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :post do
+    subject            { '社会科' }
+    course             { '日本史B' }
+    unit               { '大航海時代' }
+    teaching_materials { '山川出版社日本史B' }
+    introduction       { '導入は５分です。' }
+    introduction_time  { '5' }
+    development        { '展開は３５分です。' }
+    development_time   { '35' } 
+    summary            { 'まとめは１０分です。' }
+    summary_time       { '10' }
   end
 end
