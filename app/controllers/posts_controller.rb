40b51d2e92234b3a1db8dee34ddf3bd3213927da
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 13:02:10 +0900

    投稿の保存

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-  
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:subject, :course, :unit, :teaching_materials, :introduction, :introduction_time, :development, :development_time, :summary, :summary_time, :image).merge(user_id: current_user.id)
 end
