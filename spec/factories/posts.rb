c32d18e2475055f54a8f9269713199f180eeaec2
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-06 17:26:57 +0900

    Faker修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,11 +4,17 @@ FactoryBot.define do
     course             { '日本史B' }
     unit               { '大航海時代' }
     teaching_materials { '山川出版社日本史B' }
-    introduction       { '導入は５分です。' }
-    introduction_time  { '5' }
-    development        { '展開は３５分です。' }
-    development_time   { '35' } 
-    summary            { 'まとめは１０分です。' }
-    summary_time       { '10' }
+    introduction       { 'テストテストテストテストテストテスト' }
+    introduction_time  { 5 }
+    development        { 'テストテストテストテストテストテスト'  }
+    development_time   { 35 } 
+    summary            { 'テストテストテストテストテストテスト'  }
+    summary_time       { 10 }
+
+    association :user
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_img.png'), filename: 'test_img.png')
+    end
   end
 end
