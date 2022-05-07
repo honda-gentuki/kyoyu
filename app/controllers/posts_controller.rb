1ad1ae265e9ebf3deb9a0c6a8da789af588c0069
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-07 10:41:53 +0900

    更新内容が保存されるよう記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,7 +32,11 @@ class PostsController < ApplicationController
   end
 
   def update
-
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
   end
 
 end
