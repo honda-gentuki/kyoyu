2a41e66ef2acae3424980431fcad5fda0d0fae81
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-07 10:33:58 +0900

    投稿内容編集ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @users = User.all
@@ -23,6 +25,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
 end
 
 private
@@ -34,4 +42,8 @@ end
 
 def move_to_index
   redirect_to new_user_session_path unless user_signed_in?
+end
+
+def set_post
+  @post = Post.find(params[:id])
 end
