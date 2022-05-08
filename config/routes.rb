6ae962b532fb2e9c65d975f29dcfbd8d0939ea85
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-08 13:02:20 +0900

    Commentのルーティングとコントローラーの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
   root 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
