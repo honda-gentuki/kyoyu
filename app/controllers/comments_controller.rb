fdb4795be9b8d58a953bda576f974f25ae493d9f
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-08 14:13:57 +0900

    コメントコントローラーの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    if @comments = Comment.create(comment_params)
+      redirect_to "/posts/#{@comments.post.id}"
+    else
+      render post_path
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
