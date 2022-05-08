29e67c76e0b2fea63355ff92f8e73954d80db8db
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-08 11:58:41 +0900

    退会ボタンの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to post_path
+  end
 end
