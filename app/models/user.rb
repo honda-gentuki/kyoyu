4b6c872295f5db99dcfea92880fb6e6cc525f0f9
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-08 12:58:40 +0900

    各モデルのアソシエーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments
 
   with_options presence: true do
     validates :nickname
