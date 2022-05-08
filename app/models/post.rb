8a07427af937296c3f3d82fdec9763c786d9d14f
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-08 15:19:10 +0900

    モデルの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :image
 
   with_options presence: true do
