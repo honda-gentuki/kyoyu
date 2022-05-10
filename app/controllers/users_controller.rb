08adabe9aeeeeb8c66320bc4af01c2f0ddae7932
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-10 18:04:59 +0900

    rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     @users = user.following_user.page(params[:page]).per(3).reverse_order
   end
-  
+
   def followers
     user = User.find(params[:id])
     @users = user.follower_user.page(params[:page]).per(3).reverse_order
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :posts, through: :likes
-  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followed, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
   has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人
   has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人
 
