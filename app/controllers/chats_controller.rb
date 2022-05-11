188a8dc3e6a91099aa21ff0a58f10db22b6973fc
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-11 14:06:43 +0900

    chatsコントローラー作成

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,30 @@
+class ChatsController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    rooms = current_user.room_users.pluck(:room_id)
+    room_users = RoomUser.find_by(user_id: @user.id, room_id: rooms)
+  
+    if room_users.nil?
+      @room = Room.new
+      @room.save
+      RoomUser.create(user_id: @user.id, room_id: @room.id)
+      RoomUser.create(user_id: current_user.id, room_id: @room.id)
+    else
+      @room = room_users.room
+    end
+  
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room.id)
+  end
+  
+  def create
+    @chat = current_user.chats.new(chat_params)
+    @chat.save
+  end
+  
+  private
+  
+  def chat_params
+    params.require(:chat).permit(:message, :room_id)
+  end
+end
