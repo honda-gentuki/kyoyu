244bf5180b2239c38114ae3485ff94b1b4c69c14
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-11 10:51:36 +0900

    モデルとバリデーションの追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, dependent: :destroy
-  has_one_attached :image
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
+  has_many_attached :images
 
   def liked_by?(_user)
     likes.where(user_id: user_id).exists?
@@ -19,6 +19,7 @@ class Post < ApplicationRecord
   validates :introduction, length: { maximum: 300 }
   validates :development,  length: { maximum: 1000 }
   validates :summary,      length: { maximum: 300 }
+  validates :images, length: { maximum: 5, message: "は5枚以下にしてください" }
 
   with_options numericality: { only_integer: true, less_than_or_equal_to: 50 } do
     validates :introduction_time
