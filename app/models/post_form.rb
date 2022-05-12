b8f1ae5036b0f08e31dd5d5859316da7e2744923
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-12 14:46:32 +0900

    formオブジェクトの修正

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -2,8 +2,8 @@ class PostForm
   include ActiveModel::Model
 
   attr_accessor(
-  :subject, :course, :unit, :introduction, :introduction_time, :development, :development_time, :summary, :summary_time, :teaching_materials,
-  :id, :user_id, :created_at, :updated_at, :images
+  :subject, :course, :unit, :introduction, :introduction_time, :development, :development_time, :summary, :summary_time, :teaching_materials, :images,
+  :id, :user_id, :created_at, :updated_at, :datetime
   )
 
   with_options presence: true do
@@ -28,5 +28,8 @@ class PostForm
     Post.create(subject: subject, course: course, unit: unit, introduction: introduction, introduction_time: introduction_time, development: development, development_time: development_time,summary: summary, summary_time: summary_time, teaching_materials: teaching_materials, images: images, user_id: user_id)
   end
 
+  def update(params, post)
+    post.update(params)
+  end
 
 end
