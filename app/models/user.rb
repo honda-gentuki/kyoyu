5968d3fd30a32a2df44079ae57d0e6da62dc0453
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-14 22:06:04 +0900

    モデルの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   has_many :chats, dependent: :destroy
   has_many :active_notifications, class_name: 'Notification', foreign_key: 'visiter_id', dependent: :destroy
   has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
+  has_many :reads, dependent: :destroy
 
   def already_liked?(post)
     likes.exists?(post_id: post.id)
